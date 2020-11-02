ff69d53f242101da0a573d1f4f6ae86c26b9cfd4
Noah Demi <58009902+NoahDemi@users.noreply.github.com>
2020-11-02 16:24:04 -0500

    Add files via upload

---
+++ Assets/_Scripts/StartGameHole.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class StartGameHole : MonoBehaviour
+{
+    void Update()
+    {
+        if(Goal.goalMet == true){
+            SceneManager.LoadScene("Scene_0_");
+            
+            }
+        
+    }
+}
